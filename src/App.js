86a5929d7b23c1907d5a1c766d20019391b657f0
WinPCi9 <saldoz@ya.ru>
2021-12-26 01:31:22 +0300

    Fix: перевел счетчик в отдельный компонент

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React, {useState} from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  // Назначаем для счетчика состояние
-  // Делаем реструктуризацию
-  const [likes, setLikes] = useState(0)
   const [value, setValue] = useState('Какое-то значение')
-
-  function increment() {
-    setLikes(likes + 1)
-  }
-
-  function decrement() {
-    setLikes(likes - 1)
-  }
 
   return (
     <div className="App">
-      <h1>Likes {likes}</h1>
-      <h1>Value {value}</h1>
-      <input type="text"
-             value={value}
-             onChange={event => setValue(event.target.value)}
-      />
-      <button onClick={increment}>Up!</button>
-      <button onClick={decrement}>Down!</button>
+      <Counter/>
     </div>
   );
 }
