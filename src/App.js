3641447a72110203dc4022e5c4cc6dc1da7e84ac
WinPCi9 <saldoz@ya.ru>
2021-12-26 01:41:56 +0300

    Add: добавлены начальные стили и заголовка для отображения постов

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, {useState} from "react";
-import Counter from "./components/Counter";
+import './styles/App.css';
 
 function App() {
   const [value, setValue] = useState('Какое-то значение')
 
   return (
     <div className="App">
-      <Counter/>
-      <Counter/>
-      <Counter/>
-      <Counter/>
+      <div className="post">
+        <div className="post__content">
+          <strong>1. JavaScript</strong>
+          <div>
+            JavaScript - классный язык программирования!
+          </div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+        </div>
+
+      </div>
     </div>
   );
 }
