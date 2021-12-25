4030778618d0485a2e1c3c2d7797109b56103aae
WinPCi9 <saldoz@ya.ru>
2021-12-26 01:50:18 +0300

    Fix: выделил один пост в отдельный компонент

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React, {useState} from "react";
 import './styles/App.css';
+import PostItem from "./components/PostItem";
 
 function App() {
   const [value, setValue] = useState('Какое-то значение')
 
   return (
     <div className="App">
-      <div className="post">
-        <div className="post__content">
-          <strong>1. JavaScript</strong>
-          <div>
-            JavaScript - классный язык программирования!
-          </div>
-        </div>
-        <div className="post__btns">
-          <button>Удалить</button>
-        </div>
-
-      </div>
+      <PostItem/>
+      <PostItem/>
+      <PostItem/>
     </div>
   );
 }
