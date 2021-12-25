4b4217e844f812e02333cdf69693b3e4bd25603f
WinPCi9 <saldoz@ya.ru>
2021-12-26 01:27:27 +0300

    Add: добавлено двухстороннее связывание input-а

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   // Назначаем для счетчика состояние
   // Делаем реструктуризацию
   const [likes, setLikes] = useState(0)
+  const [value, setValue] = useState('Какое-то значение')
 
   function increment() {
     setLikes(likes + 1)
@@ -16,6 +17,11 @@ function App() {
   return (
     <div className="App">
       <h1>Likes {likes}</h1>
+      <h1>Value {value}</h1>
+      <input type="text"
+             value={value}
+             onChange={event => setValue(event.target.value)}
+      />
       <button onClick={increment}>Up!</button>
       <button onClick={decrement}>Down!</button>
     </div>
