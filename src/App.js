573d809a7eb4985dfb5d2c75584e4e7c06aa59e4
WinPCi9 <saldoz@ya.ru>
2021-12-26 23:43:09 +0300

    Fix: создан отдельный компонент для поиска и сортировки, которую мы реализовали ранее

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PostsList from "./components/PostsList";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import MyInput from "./components/UI/input/MyInput";
+import PostFilter from "./components/PostFilter";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -12,25 +13,24 @@ function App() {
     {id: 3, title: "Python", body: "Любимый язык"},
     {id: 4, title: "Yoohooo", body: "Текст поста"},
   ])
-  const [selectedSort, setSelectedSort] = useState('')
-  const [searchQuery, setSearchQuery] = useState('')
+  const [filter, setFilter] = useState({sort: '', query: ''})
 
   // хук useMemo кэширует состояние переменной и перечитывает это значение только тогда,
   // когда изменились dependencies (deps) - свойства, т.е. второй аргумент хука useMemo
   const sortedPosts = useMemo(() => {
     // Если не пустая строка
-    if (selectedSort) {
+    if (filter.sort) {
       // Сортируем копию массива - для этого надо его развернуть ([...posts])
       // Для сортировки используется функция localeCompare, которая сравнивает строки
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/localeCompare
-      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
     }
     return posts
-  }, [selectedSort, posts])
+  }, [filter.sort, posts])
 
   const sortedAdnSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery.toLowerCase()))
-  }, [searchQuery, sortedPosts])
+    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+  }, [filter.query, sortedPosts])
 
   const createPost = (newPost) => {
     setPosts([...posts,  newPost])
@@ -42,31 +42,12 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
-  const sortPosts = (sort) => {
-    // sort содержит либо поле title, либо body
-    setSelectedSort(sort);
-  }
-
   return (
     <div className="App">
       <PostForm create={createPost}/>
       <hr style={{margin: '15px 0'}}/>
-      <div>
-        <MyInput
-          value={searchQuery}
-          onChange={e => setSearchQuery(e.target.value)}
-          placeholder="Поиск..."
-        />
-        <MySelect
-          value={selectedSort}
-          onChange={sortPosts}
-          defaultValue="Сортировка по..."
-          options={[
-            {value: 'title', name: 'По названию'},
-            {value: 'body', name: 'По описанию'},
-          ]}
-        />
-      </div>
+      <PostFilter filter={filter} setFilter={setFilter}/>
+
       {sortedAdnSearchedPosts.length !== 0
         ? <PostsList remove={removePost} posts={sortedAdnSearchedPosts} title="Список постов"/>
         : <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
